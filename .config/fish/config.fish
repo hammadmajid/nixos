3a95590234fc0fa87b9918dc46243d95f85f3f35
Hammad Majid <hammadmajid@proton.me>
2024-07-24 21:12:09 +0500

    Format and organize code in fish config file and explain stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,51 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    eval (zellij setup --generate-auto-start fish | string collect)
-end
-
 function fish_greeting
+    # A tux greets you with random fortune
+    # Packages: fortune-mod cowsay
     fortune | cowsay -f tux
 end
 
-set EDITOR helix
+if status is-interactive
+    # Run zellij on shell startup
+    # It won't start zellij inside zellij when you create new tab
+    # or pane
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
 
+# Enviroment variables
+set EDITOR helix # package: helix
+
+# Aliases for commonly used commands
+# ---------------------------------- 
+#
+# If you want to use the default ls run `\ls`
+# Package: exa
 alias ls="exa --icons --group-directories-first"
-alias e="$EDITOR"
+#
+# Package: lazygit
+alias gg="lazygit"
+#
+# Best used when piping into this alias
+# Package: xclip
+alias copy="xclip -selection clipboard"
+#
+# Package: sl
+alias claer=sl
+#
 alias cdd="cd ~/Dev"
 alias hx="$EDITOR"
-alias gg="lazygit"
-alias copy="xclip -selection clipboard" # Pipe it
-alias claer=sl
 
+# Shell integration 
+# ------------------
+#
+# To use regular cd run it using `\cd`
+# Package: zoxide 
 zoxide init --cmd cd fish | source
+#
+# Package: starship
 starship init fish | source
+#
+# Package: mise
+mise activate fish | source
 
 # pnpm
 set -gx PNPM_HOME "/home/hammad/.local/share/pnpm"
@@ -26,4 +53,3 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-mise activate fish | source
