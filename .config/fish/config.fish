ea59919f9ae52a67de58c420c69404683bb88d27
Hammad Majid <hammadmajid@proton.me>
2024-09-03 09:17:24 +0500

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+function fish_greeting
+end
+
+if status is-interactive
+end
+
+set -Ux EDITOR helix
+
+# Aliases for commonly used commands
+# ------------------
+
+alias ls="exa --icons --group-directories-first"
+alias copy="xclip -selection clipboard"
+alias cdd="cd ~/Dev"
+alias shx="sudo hx"
+alias bnd="bun run dev"
+alias bnb="bun run build"
+alias bnp="bun run preview"
+
+# Shell integration
+# ------------------
+
+# mise activate fish | source
+zoxide init --cmd cd fish | source
+atuin init fish --disable-up-arrow | source
+
+starship init fish | source
+function starship_transient_prompt_func
+    starship module character
+end
+starship init fish | source
+enable_transience
