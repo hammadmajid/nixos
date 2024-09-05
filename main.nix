7a910a52ebb158a78a860660d634d6c07796fad4
Hammad Majid <hammadmajid@proton.me>
2024-09-05 15:19:17 +0500

    33 current  2024-09-05 15:11:24  24.05.4469.6e99f2a27d60  6.6.48                          *

--- main.nix
+++ main.nix
@@ -99,6 +99,7 @@
       fd
       gh
       just
+      kanata
       lazygit
       rclone
       ripgrep
@@ -177,6 +178,19 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+
+  # Enable kanata service
+  services.kanata.enable = true;
+  services.kanata.keyboards = {
+    config = ''
+      (defsrc
+        caps)
+
+      (deflayermap (default-layer)
+      ;; tap caps lock as caps lock, hold caps lock as left control
+      caps (tap-hold 100 100 caps lctl))
+    '';
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
