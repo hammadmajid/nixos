d3d29fe734b2fda7d6cca1c99883f443333cedca
Hammad Majid <hammadmajid@proton.me>
2024-09-08 14:48:47 +0500

    init nix flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "System config";
+
+  inputs = {
+    unstable-nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    unstable-nixpkgs,
+  }:
+  let
+    unstable = unstable-nixpkgs.legacyPackages.x86_64-linux;
+  in
+   {
+    packages.x86_64-linux.default = unstable.hello;
+  };
+}
